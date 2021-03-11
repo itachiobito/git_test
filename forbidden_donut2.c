72433c7d7d3bd96d19cda6886668a17722693a41
itachiobito <zitruspresse@gmx.de>
2021-03-11 21:43:36 +0100

    Bis Zeile 13 abgeschrieben

--- forbidden_donut2.c
+++ forbidden_donut2.c
@@ -3,4 +3,13 @@ double sin(),cos();
 main()
 {
 float A=0, B=0,i,j,z[1760];
+char b[1760];
+printf("\x1b[2J");
+for(;;){
+  memset(b,32,1760);
+  memset(z,0,7040);
+  for(j=0;6.28>j;j+=0.07)
+  for(i=0;6.28>i;i+=0.02){
+    float c=sin(i),d=cos(j),e=sin(A),f=sin(j),g=cos(A),h=d+2,D=1/(c*h*e*+f*g+5),l=cos(i),m=cos(B),n=sin(B),t=c*h*g-f*e;}
+}
 }

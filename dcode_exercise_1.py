f7a59321c702f8df53f527596b557422db6da4f1
itachiobito <zitruspresse@gmx.de>
2021-06-29 18:31:52 +0200

    first dcode excercise; manipulate arrays

---
+++ dcode_exercise_1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+https://stackoverflow.com/questions/11170827/how-do-i-tell-a-python-script-to-use-a-particular-version
+https://en.wikipedia.org/wiki/Shebang_(Unix)
+"""
+
+x = [1,2,3,4,5]
+index = x[0]
+
+for i in x:
+
+    print(index)
+    print(type(i),i)
